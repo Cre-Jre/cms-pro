22cea14a924176264dc37b34984d5e2aaa607828
luoyurui <835507025@qq.com>
2021-03-25 11:28:45 +0800

    103

--- cms-portal/src/main/java/com/cms/portal/controller/admin/RoleController.java
+++ cms-portal/src/main/java/com/cms/portal/controller/admin/RoleController.java
@@ -1,11 +1,26 @@
 package com.cms.portal.controller.admin;
 
+import com.cms.contex.foundation.Result;
+import com.cms.contex.utils.UtilsTemplate;
+import com.cms.service.dto.CmsRoleDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("role")
 public class RoleController {
 
+    @GetMapping("index.do")
+    public String toIndex(){
+        return UtilsTemplate.adminTemplate("role","index");
+    }
 
+    @PostMapping("page.do")
+    @ResponseBody
+    public Result doPage(CmsRoleDto cmsRoleDto){
+        return Result.success();
+    }
 }
