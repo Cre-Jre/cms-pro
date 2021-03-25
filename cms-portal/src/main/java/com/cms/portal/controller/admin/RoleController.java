d50b0701ec97cbcb41bff1c8895e1d34a33dec3c
luoyurui <835507025@qq.com>
2021-03-25 13:22:27 +0800

    105

--- cms-portal/src/main/java/com/cms/portal/controller/admin/RoleController.java
+++ cms-portal/src/main/java/com/cms/portal/controller/admin/RoleController.java
@@ -2,16 +2,22 @@ package com.cms.portal.controller.admin;
 
 import com.cms.contex.foundation.Result;
 import com.cms.contex.utils.UtilsTemplate;
+import com.cms.service.api.CmsPermissionService;
+import com.cms.service.dto.CmsPermissionDto;
 import com.cms.service.dto.CmsRoleDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("role")
 public class RoleController {
+
+    @Autowired
+    private CmsPermissionService cmsPermissionService;
 
     @GetMapping("index.do")
     public String toIndex(){
@@ -22,5 +28,17 @@ public class RoleController {
     @ResponseBody
     public Result doPage(CmsRoleDto cmsRoleDto){
         return Result.success();
+    }
+
+    @GetMapping("add.do")
+    public String toAdd(){
+        return UtilsTemplate.adminTemplate("role","add");
+    }
+
+    @PostMapping("permission.do")
+    @ResponseBody
+    public Result doPermission(){
+        ArrayList<CmsPermissionDto> list = (ArrayList)cmsPermissionService.getList(null);
+        return Result.success(list);
     }
 }
