41d901e2b46df89476b7cd779e5d9d3dda74f3f1
luoyurui <835507025@qq.com>
2021-03-28 20:14:17 +0800

    152

---
+++ cms-portal/src/main/java/com/cms/portal/freemarker/directive/CmsFriendLinkDirective.java
@@ -0,0 +1,19 @@
+package com.cms.portal.freemarker.directive;
+
+import freemarker.core.Environment;
+import freemarker.template.TemplateDirectiveBody;
+import freemarker.template.TemplateDirectiveModel;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateModel;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class CmsFriendLinkDirective implements TemplateDirectiveModel {
+
+
+    @Override
+    public void execute(Environment environment, Map map, TemplateModel[] templateModels, TemplateDirectiveBody templateDirectiveBody) throws TemplateException, IOException {
+
+    }
+}
