743f11b443569668de19980ac22f7f453477db2d
luoyurui <835507025@qq.com>
2021-03-28 10:57:20 +0800

    125

--- cms-portal/src/main/resources/migration/V1__Base_version.sql
+++ cms-portal/src/main/resources/migration/V1__Base_version.sql
@@ -1,69 +1,32 @@
 -- ----------------------------
---  Table structure for cms_user_primary 用户主表  密码: a1234567
--- ----------------------------
-CREATE TABLE cms_user_primary
-(
-    create_time timestamp   not null default CURRENT_TIMESTAMP,
-    update_time timestamp   not null default '0000-00-00 00:00:00',
-    id          int(11)     NOT NULL AUTO_INCREMENT primary key,
-    username    varchar(50) not null comment '用户名',
-    password    varchar(64) not null comment '用户密码，MD5加密或sha256散列加密',
-    salt        varchar(64) not null comment '密码盐',
-    email       varchar(50) not null default '' comment '邮箱',
-    login_count int(10)     not null default 0 comment '登陆次数',
-    UNIQUE KEY user_name_unique (username) USING BTREE,
-    UNIQUE KEY user_email_unique (email) USING BTREE
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
-INSERT INTO cms_user_primary (id, create_time, update_time, username, password, salt, email, login_count)
-VALUES (1, '2019-06-14 11:30:58', null, 'admin', 'e298f9b29585da289080ffebb32e6f931b52a61195bcf6246d3e0f24654897eb',
-        '6e4abc9695661ce11f442eaea3cb6540', 'abc@126.com', 0);
-INSERT INTO cms_user_primary (id, create_time, update_time, username, password, salt, email, login_count)
-VALUES (2, '2019-06-14 11:30:58', null, 'administrator',
-        'e298f9b29585da289080ffebb32e6f931b52a61195bcf6246d3e0f24654897eb', '6e4abc9695661ce11f442eaea3cb6540',
-        'def@126.com', 0);
-
--- ----------------------------
---  Table structure for cms_user  用户副表
+--  Table structure for cms_user 用户表  密码: a1234567
 -- ----------------------------
 CREATE TABLE cms_user
 (
-    create_time       timestamp    not null default CURRENT_TIMESTAMP,
-    update_time       timestamp    not null default '0000-00-00 00:00:00',
-    id                int(11)      NOT NULL AUTO_INCREMENT primary key,
-    group_id          int          null comment '会员组ID',
-    username          varchar(50)  not null comment '用户名',
-    email             varchar(50)  not null default '' comment '邮箱',
-    rank              int                   default '0' not null comment '管理员级别',
-    is_admin          tinyint(1)   not null default 0 comment '是否后台用户 0不是 1是',
-    is_super          tinyint(1)   not null default 0 comment '是否超级管理员 0不是 1是',
-    is_self_admin     tinyint(1)            default '0' not null comment '是否受限管理员 是否只管理自己的数据 0:否 1:是',
-    register_time     timestamp    not null comment '注册时间',
-    register_ip       varchar(50)           default '127.0.0.1' not null comment '注册IP',
-    login_count       int(10)      not null default 0 comment '登陆次数',
-    upload_total      bigint                default '0' not null comment '上传总大小',
-    upload_size       int                   default '0' not null comment '上传大小',
-    is_viewonly_admin tinyint(1)            default '0' not null comment '是否只读管理员 0:否 1:是',
-    upload_date       timestamp    null comment '上传日期',
-    session_id        varchar(200) not null default '' comment '用户session id',
-    status            tinyint(1)            default '1' not null comment '状态',
-    deleted           tinyint(1)            default '1' not null comment '是否已删除 0:删除 1正常',
-    last_login_ip     varchar(30)  not null default '' comment '最后登录ip',
-    last_login_time   timestamp    not null default '0000-00-00 00:00:00' comment '最后登录时间',
+    create_time   timestamp   not null default CURRENT_TIMESTAMP,
+    update_time   timestamp   not null default '0000-00-00 00:00:00',
+    id            int(11)     NOT NULL AUTO_INCREMENT primary key,
+    username      varchar(50) not null comment '用户名',
+    password      varchar(64) not null comment '用户密码，MD5加密或sha256散列加密',
+    salt          varchar(64) not null comment '密码盐',
+    email         varchar(50) not null default '' comment '邮箱',
+    login_count   int(10)     not null default 0 comment '登陆次数',
+    is_backend    tinyint(1)  not null default 0 comment '是否后台用户 0不是 1是',
+    is_super      tinyint(1)  not null default 0 comment '是否超级管理员 0不是 1是',
+    register_time timestamp   not null comment '注册时间',
+    register_ip   varchar(50)          default '127.0.0.1' not null comment '注册IP',
+    status        tinyint(1)           default '1' not null comment '状态 是否禁用 0:禁用 1:正常',
+    is_delete     tinyint(1)           default '1' not null comment '是否已删除 0:删除 1正常',
     UNIQUE KEY user_name_unique (username) USING BTREE,
     UNIQUE KEY user_email_unique (email) USING BTREE
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
-INSERT INTO cms_user (id, create_time, update_time, group_id, username, email, rank, is_admin, is_super, is_self_admin,
-                      register_time, register_ip, login_count, upload_total, upload_size, is_viewonly_admin,
-                      upload_date, session_id, last_login_ip, last_login_time)
-VALUES (1, '2019-06-14 11:30:58', null, 1, 'admin', 'abc@126.com', 1, 1, 1, 0, '2019-06-14 11:30:58', '127.0.0.1', 0, 0,
-        0, 0, null, '', '', null);
-INSERT INTO cms_user(id, create_time, update_time, group_id, username, email, rank, is_admin, is_self_admin,
-                     register_time, register_ip, login_count, upload_total, upload_size, is_viewonly_admin, upload_date,
-                     session_id, last_login_ip, last_login_time)
-VALUES (2, '2019-06-14 11:30:58', null, 1, 'administrator', 'def@126.com', 1, 1, 0, '2019-06-14 11:30:58', '127.0.0.1',
-        0, 0, 0, 0, null, '', '', null);
+INSERT INTO cms_user (id, create_time, update_time, username, password, salt, email, login_count, is_backend, is_super,
+                      register_time, register_ip)
+VALUES (1, '2019-06-14 11:30:58', null, 'admin', 'e298f9b29585da289080ffebb32e6f931b52a61195bcf6246d3e0f24654897eb',
+        '6e4abc9695661ce11f442eaea3cb6540', 'abc@126.com', 0, 1, 1, '2019-06-14 11:30:58', '127.0.0.1');
+
+
 
 -- ----------------------------
 --  Table structure for cms_log  日志表
@@ -74,7 +37,7 @@ CREATE TABLE cms_log
     update_time timestamp    not null default '0000-00-00 00:00:00',
     id          int(11)      NOT NULL AUTO_INCREMENT primary key,
     user_id     int(11)      not null comment '用户id',
-    username varchar(25) not null comment '用户名称',
+    username    varchar(25)  not null comment '用户名称',
     login_ip    varchar(30)           default '' comment 'ip地址',
     url         varchar(100)          default '' comment 'URL地址',
     content     varchar(100) null comment '日志内容'
@@ -86,32 +49,33 @@ CREATE TABLE cms_log
 -- ----------------------------
 CREATE TABLE cms_site
 (
-    create_time        timestamp                   not null default CURRENT_TIMESTAMP,
-    update_time        timestamp                   not null default '0000-00-00 00:00:00',
-    id                 int(11)                     NOT NULL AUTO_INCREMENT primary key,
-    site_name          varchar(100)                not null comment '网站名称',
-    keywords           varchar(255)                not null default '' comment '站点关键字',
-    description        varchar(255)                not null default '' comment '站点描述'
+    create_time timestamp    not null default CURRENT_TIMESTAMP,
+    update_time timestamp    not null default '0000-00-00 00:00:00',
+    id          int(11)      NOT NULL AUTO_INCREMENT primary key,
+    site_name   varchar(100) not null comment '网站名称',
+    keywords    varchar(255) not null default '' comment '站点关键字',
+    description varchar(255) not null default '' comment '站点描述'
 ) ENGINE = InnoDB
   AUTO_INCREMENT = 2
   DEFAULT CHARSET = utf8 comment ='CMS站点表';
-INSERT INTO cms_site (id, site_name, keywords, description) VALUES (1, 'cms官网','cms,内容管理', 'cms内容管理系统');
+INSERT INTO cms_site (id, site_name, keywords, description)
+VALUES (1, 'cms官网', 'cms,内容管理', 'cms内容管理系统');
 
 -- ----------------------------
 -- Table structure for cms_permission  权限表
 -- ----------------------------
 CREATE TABLE cms_permission
 (
-    create_time timestamp    not null default CURRENT_TIMESTAMP,
-    update_time timestamp    not null default '0000-00-00 00:00:00',
-    id          int(11)      NOT NULL AUTO_INCREMENT primary key,
-    parent_id   int(11)      default 0 comment '父级id',
-    is_menu     tinyint(1)   default 0 comment '是否菜单 0:否 1:是',
-    icon        varchar(30)  default '' comment '菜单图标',
-    name varchar(25) not null comment '权限名称',
-    url         varchar(50) default '' comment '链接地址',
-    action      varchar(100)  default '' comment '权限码',
-    priority    int(11) NOT NULL comment '排序'
+    create_time timestamp   not null default CURRENT_TIMESTAMP,
+    update_time timestamp   not null default '0000-00-00 00:00:00',
+    id          int(11)     NOT NULL AUTO_INCREMENT primary key,
+    parent_id   int(11)              default 0 comment '父级id',
+    is_menu     tinyint(1)           default 0 comment '是否菜单 0:否 1:是',
+    icon        varchar(30)          default '' comment '菜单图标',
+    name        varchar(25) not null comment '权限名称',
+    url         varchar(50)          default '' comment '链接地址',
+    action      varchar(100)         default '' comment '权限码',
+    priority    int(11)     NOT NULL comment '排序'
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
@@ -120,13 +84,13 @@ CREATE TABLE cms_permission
 -- ----------------------------
 CREATE TABLE cms_role
 (
-    create_time timestamp               not null default CURRENT_TIMESTAMP,
-    update_time timestamp               not null default '0000-00-00 00:00:00',
-    id          int(11)                 NOT NULL AUTO_INCREMENT primary key,
-    name        varchar(50)             not null default '' comment '权限名称',
-    priority    int        default '1' not null comment '排列顺序',
-    is_all      tinyint(1) default 0    not null comment '是否所有权限 0.否 1.是',
-    status      tinyint(1) default 0  not null comment '状态 0:正常 1:禁用'
+    create_time timestamp   not null default CURRENT_TIMESTAMP,
+    update_time timestamp   not null default '0000-00-00 00:00:00',
+    id          int(11)     NOT NULL AUTO_INCREMENT primary key,
+    name        varchar(50) not null default '' comment '权限名称',
+    priority    int                  default '1' not null comment '排列顺序',
+    is_all      tinyint(1)           default 0 not null comment '是否所有权限 0.否 1.是',
+    status      tinyint(1)           default 0 not null comment '状态 0:正常 1:禁用'
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
@@ -135,7 +99,7 @@ CREATE TABLE cms_role
 -- ----------------------------
 CREATE TABLE cms_role_permission
 (
-    role_id    int         not null comment '角色id',
+    role_id       int not null comment '角色id',
     permission_id int not null comment '权限id'
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
