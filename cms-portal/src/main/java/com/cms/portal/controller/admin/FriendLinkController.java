26468db2605d43f52bb390044b88d1b0af28d84b
luoyurui <835507025@qq.com>
2021-03-28 19:54:59 +0800

    147

--- cms-portal/src/main/java/com/cms/portal/controller/admin/FriendLinkController.java
+++ cms-portal/src/main/java/com/cms/portal/controller/admin/FriendLinkController.java
@@ -1,13 +1,22 @@
 package com.cms.portal.controller.admin;
 
+import com.cms.contex.foundation.Result;
 import com.cms.contex.utils.UtilsTemplate;
+import com.cms.service.api.CmsFriendLinkService;
+import com.cms.service.dto.CmsFriendLinkDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("friendLink")
 public class FriendLinkController {
+
+    @Autowired
+    private CmsFriendLinkService cmsFriendLinkService;
 
     @GetMapping("index.do")
     public String toIndex(){
@@ -17,5 +26,12 @@ public class FriendLinkController {
     @GetMapping("add.do")
     public String toAdd(){
         return UtilsTemplate.adminTemplate("friend","add");
+    }
+
+    @PostMapping("add.do")
+    @ResponseBody
+    public Result<String> doAdd(CmsFriendLinkDto cmsFriendLinkDto){
+        cmsFriendLinkService.save(cmsFriendLinkDto);
+        return Result.success();
     }
 }
--- cms-service/src/main/java/com/cms/service/impl/CmsFriendLinkServiceImpl.java
+++ cms-service/src/main/java/com/cms/service/impl/CmsFriendLinkServiceImpl.java
@@ -1,15 +1,22 @@
 package com.cms.service.impl;
 
 import com.cms.core.foundation.Page;
+import com.cms.dao.mapper.CmsFriendLinkMapper;
 import com.cms.service.api.CmsFriendLinkService;
+import com.cms.service.converter.CmsFriendLinkConverter;
 import com.cms.service.dto.CmsFriendLinkDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CmsFriendLinkServiceImpl implements CmsFriendLinkService {
+
+    @Autowired
+    private CmsFriendLinkMapper cmsFriendLinkMapper;
+
     @Override
     public void save(CmsFriendLinkDto dto) {
-
+        cmsFriendLinkMapper.save(CmsFriendLinkConverter.CONVERTER.dtoToEntity(dto));
     }
 
     @Override
