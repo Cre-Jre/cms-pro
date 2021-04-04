18e58a4945ac147f3a7e8e3b79c5c512781396ab
luoyurui <835507025@qq.com>
2021-04-04 17:41:25 +0800

    182

--- cms-service/src/main/java/com/cms/service/impl/CmsTaskServiceImpl.java
+++ cms-service/src/main/java/com/cms/service/impl/CmsTaskServiceImpl.java
@@ -1,9 +1,11 @@
 package com.cms.service.impl;
 
 import com.cms.contex.utils.UtilsString;
+import com.cms.core.exception.BusinessException;
 import com.cms.core.foundation.Page;
 import com.cms.dao.entity.CmsTaskEntity;
 import com.cms.dao.enums.TaskExecutionCycleUnitEnum;
+import com.cms.dao.enums.TaskExecutionTypeEnum;
 import com.cms.dao.enums.TaskStaticTypeEnum;
 import com.cms.dao.mapper.CmsTaskMapper;
 import com.cms.service.api.CmsTaskService;
@@ -11,17 +13,28 @@ import com.cms.service.converter.CmsTaskConverter;
 import com.cms.service.dto.CmsTaskDto;
 import com.cms.service.strategy.*;
 import com.cms.service.task.job.IndexStaticJob;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.BooleanUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
+import org.springframework.scheduling.quartz.JobDetailFactoryBean;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 import org.springframework.stereotype.Service;
 
+import java.text.ParseException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
+@Slf4j
 @Service
 public class CmsTaskServiceImpl implements CmsTaskService {
 
+    @Autowired
+    private Scheduler scheduler;
     @Autowired
     private CmsTaskMapper cmsTaskMapper;
 
@@ -41,7 +54,7 @@ public class CmsTaskServiceImpl implements CmsTaskService {
      * 执行具体任务的job Map
      * @param dto
      */
-    private static final Map<TaskStaticTypeEnum,Class<? extends QuartzJobBean>> task_job_class_map = new HashMap<TaskStaticTypeEnum,Class<? extends QuartzJobBean>>(){{
+    private static final Map<TaskStaticTypeEnum,Class<? extends QuartzJobBean>> TASK_JOB_CLASS_MAP = new HashMap<TaskStaticTypeEnum,Class<? extends QuartzJobBean>>(){{
         put(TaskStaticTypeEnum.INDEX, IndexStaticJob.class);
     }};
 
@@ -56,7 +69,27 @@ public class CmsTaskServiceImpl implements CmsTaskService {
     }
 
     public void startTask(CmsTaskDto cmsTaskDto){
+        TaskExecutionTypeEnum taskExecutionType = cmsTaskDto.getTaskExecutionType();
+        String cronExpression = Objects.equals(taskExecutionType, TaskExecutionTypeEnum.EXECUTION_MODE) ? cmsTaskDto.getCronExpression() : TASK_CRON_EXPRESSION.get(cmsTaskDto.getIntervalUnit()).buildCronExpress(cmsTaskDto);
+        log.info("cronExpression表达式=[{}]",cronExpression);
+        if(StringUtils.contains(cronExpression,"null")){
+            return;
+        }
+        JobDetailFactoryBean jobDetailFactoryBean = new JobDetailFactoryBean();
+        jobDetailFactoryBean.setName(cmsTaskDto.getCode());
+        jobDetailFactoryBean.setJobClass(TASK_JOB_CLASS_MAP.get(cmsTaskDto.getType()));
+        jobDetailFactoryBean.afterPropertiesSet();
 
+        CronTriggerFactoryBean cronTriggerFactoryBean = new CronTriggerFactoryBean();
+        cronTriggerFactoryBean.setCronExpression(cronExpression);
+        cronTriggerFactoryBean.setName(cmsTaskDto.getName()+cmsTaskDto.getCode());
+        try {
+            cronTriggerFactoryBean.afterPropertiesSet();
+            scheduler.scheduleJob(jobDetailFactoryBean.getObject(),cronTriggerFactoryBean.getObject());
+        } catch (Exception e) {
+            log.error("执行定时任务失败,message=[{}]",e.getMessage());
+            throw new BusinessException(e.getMessage());
+        }
     }
 
 
