bab27e66336572b7f508ea26a7609824d69afbc3
luoyurui <835507025@qq.com>
2021-04-04 15:49:28 +0800

    162

--- cms-portal/src/main/java/com/cms/portal/controller/admin/StaticPageController.java
+++ cms-portal/src/main/java/com/cms/portal/controller/admin/StaticPageController.java
@@ -1,15 +1,24 @@
 package com.cms.portal.controller.admin;
 
 import com.cms.contex.foundation.Result;
+import com.cms.contex.utils.UtilsTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("static")
 public class StaticPageController {
 
     @GetMapping("index.do")
+    public String toIndex(){
+        return UtilsTemplate.adminTemplate("static","index");
+    }
+
+    @PostMapping("index.do")
+    @ResponseBody
     public Result<String> doIndexStatic(){
 
         return Result.success();
