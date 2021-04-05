ba9b24e71739bd5c39a5cf9baf7c035ec88cdd3b
luoyurui <835507025@qq.com>
2021-04-05 15:23:38 +0800

    207

--- cms-portal/src/main/java/com/cms/portal/controller/admin/ModelController.java
+++ cms-portal/src/main/java/com/cms/portal/controller/admin/ModelController.java
@@ -1,13 +1,22 @@
 package com.cms.portal.controller.admin;
 
+import com.cms.contex.foundation.Result;
 import com.cms.contex.utils.UtilsTemplate;
+import com.cms.service.api.CmsModelService;
+import com.cms.service.dto.CmsModelDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("model")
 public class ModelController {
+
+    @Autowired
+    private CmsModelService cmsModelService;
 
     @GetMapping("index.do")
     public String toIndex(){
@@ -19,5 +28,10 @@ public class ModelController {
         return UtilsTemplate.adminTemplate("model","add");
     }
 
-
+    @PostMapping("add.do")
+    @ResponseBody
+    public Result<String> doAdd(CmsModelDto dto){
+        cmsModelService.save(dto);
+        return Result.success();
+    }
 }
--- cms-service/src/main/java/com/cms/service/impl/CmsModelServiceImpl.java
+++ cms-service/src/main/java/com/cms/service/impl/CmsModelServiceImpl.java
@@ -1,15 +1,22 @@
 package com.cms.service.impl;
 
 import com.cms.core.foundation.Page;
+import com.cms.dao.mapper.CmsModelMapper;
 import com.cms.service.api.CmsModelService;
+import com.cms.service.converter.CmsModelConverter;
 import com.cms.service.dto.CmsModelDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CmsModelServiceImpl implements CmsModelService {
+
+    @Autowired
+    private CmsModelMapper cmsModelMapper;
+
     @Override
     public void save(CmsModelDto dto) {
-
+        cmsModelMapper.save(CmsModelConverter.CONVERTER.dtoToEntity(dto));
     }
 
     @Override
